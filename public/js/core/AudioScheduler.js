c4ee6c644e79b0044c7aee4203453f5e5d1d4127
Miguel Antonio Sepúlveda <miseal@gmail.com>
2025-12-28 23:25:52 -0300

    debug: Add pedal CC64 logging on receiver side (AudioScheduler)

--- public/js/core/AudioScheduler.js
+++ public/js/core/AudioScheduler.js
@@ -149,6 +149,12 @@ export class AudioScheduler {
             this.stateManager.handleNoteOff(data1, 'REMOTE');
         } else if (isCC) {
             // CONTROL CHANGE: Enviar directamente al hardware (pedal, volumen, etc.)
+            
+            // Debug pedal sustain
+            if (data1 === 64) {
+                console.log(`[AudioScheduler] 🎹 Pedal CC64 recibido: value=${data2} (${data2 >= 64 ? 'DOWN' : 'UP'})`);
+            }
+            
             if (this.outputManager) {
                 this.outputManager.send(status, data1, data2, 'REMOTE');
             } else if (this.midiOutput) {
