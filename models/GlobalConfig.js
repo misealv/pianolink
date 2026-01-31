d9e27a9842effa0b408ff561c7030a5754dfa507
Miguel Antonio Sepúlveda <miseal@gmail.com>
2026-01-31 14:38:20 -0300

    Eliminada funcionalidad de Identidad/Platform del admin panel

--- models/GlobalConfig.js
+++ models/GlobalConfig.js
@@ -5,12 +5,6 @@ const globalConfigSchema = new mongoose.Schema({
     // Usamos un ID fijo o un campo 'active' para asegurar que solo haya una configuración
     isDefault: { type: Boolean, default: true, unique: true },
     
-    platformName: { type: String, default: "Piano Link" },
-    
-    // Identidad Visual
-    logoUrl: { type: String, default: "" }, // Logo Principal (Header)
-    faviconUrl: { type: String, default: "" }, // Icono de pestaña
-    
     // Tracking Pixels (Facebook Pixel & Google Analytics)
     trackingScripts: {
         facebookPixel: { type: String, default: "" }, // Script completo de Facebook Pixel
